a16d06a558229f859e8c28054b35f6ff19c83ab7
Jacob Boykin <boykinmusic@gmail.com>
2016-06-28 20:29:05 -0400

    Pass Two Tests
    Pass first two tests. Two final tests are failing

--- Intermediate Algorithm Scripting/04 - Wherefore Art Thou/solution.js
+++ Intermediate Algorithm Scripting/04 - Wherefore Art Thou/solution.js
@@ -4,7 +4,12 @@ function whatIsInAName(collection, source) {
   // Only change code below this line
 
   for (var i = 0; i < collection.length; i++) {
-    for (var key in collection[i]) {
+    for (var collProp in collection[i]) {
+      if (source.hasOwnProperty(collProp)) {
+        if (collection[i][collProp] === source[collProp]) {
+          arr.push(collection[i]);
+        }
+      }
     }
   }
 
@@ -12,4 +17,10 @@ function whatIsInAName(collection, source) {
   return arr;
 }
 
-console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+//console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+
+//console.log(whatIsInAName([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 }));
+
+console.log(whatIsInAName([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 }));
+
+console.log(whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 }));
