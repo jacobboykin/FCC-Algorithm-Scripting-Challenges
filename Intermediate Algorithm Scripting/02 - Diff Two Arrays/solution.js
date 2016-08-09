18ccf00e39c921217f1799c7eb5a9fd0ac78ba53
Jacob Boykin <boykinmusic@gmail.com>
2016-08-08 21:11:10 -0400

    Remove Console Log Tests

--- Intermediate Algorithm Scripting/02 - Diff Two Arrays/solution.js
+++ Intermediate Algorithm Scripting/02 - Diff Two Arrays/solution.js
@@ -3,27 +3,16 @@ function diffArray(arr1, arr2) {
 
   for (var i = 0; i < arr2.length; i++) {
       if ((arr1.indexOf(arr2[i])) === -1) {
-          console.log("Didn't find " + arr2[i] + " inside of arr1");
           newArr.push(arr2[i]);
-      }
-      else {
-          console.log("Found " + arr2[i]);
-
       }
   }
 
   for (var i = 0; i < arr1.length; i++) {
       if ((arr2.indexOf(arr1[i])) === -1) {
-          console.log("Didn't find " + arr1[i] + " inside of arr2");
           newArr.push(arr1[i]);
       }
-      else {
-          console.log("Found " + arr1[i]);
-
-      }
   }
-
-  console.log(newArr);
+  
   return newArr;
 }
 
